ba72b09688b1cd11c3ca032590c8222ade21a686
Justin <mhrjastin@gmail.com>
2024-09-12 16:16:14 +0545

    Initial commit

---
+++ Basic of python/day1.py
@@ -0,0 +1,36 @@
+# print("hello world")
+#
+# print(424)
+#
+# a=5
+# b=4
+# print(a,b)
+
+#define the variable called name
+# name ="qa class"
+# print(name)
+# print("Name:",name)
+# print("first line \n")
+# print("second line")
+
+#comments=> to describe the bunch of code.
+
+#3 operators
+# operators is a symbol to carry the arthmatical or logical operators
+
+# a=5
+# b=10
+# sum= a+b
+# sub= a-b
+# mul= a*b
+# div= a/b
+# print(sum)
+# print(sub)
+# print(mul)
+# print(div)
+
+a=2
+b=4
+c=5
+sum=a+b+c
+print("sum is ",sum)
