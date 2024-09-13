0e70d7b6f75793b0b47863971957104a5ccea5d8
Justin <mhrjastin@gmail.com>
2024-09-13 16:24:31 +0545

    add basic demo1

--- Basic of python/day1.py
+++ Basic of python/day1.py
@@ -5,6 +5,7 @@
 # a=5
 # b=4
 # print(a,b)
+from cgitb import reset
 
 #define the variable called name
 # name ="qa class"
@@ -29,8 +30,35 @@
 # print(mul)
 # print(div)
 
-a=2
-b=4
-c=5
-sum=a+b+c
-print("sum is ",sum)
+# a=2
+# b=4
+# c=5
+# sum=a+b+c
+# print("sum is ",sum)
+
+
+#5 concatination of string
+# first_name= "justin"
+# last_name= "maharjan"
+#
+# fullname= first_name+ ""+ last_name
+# print(fullname)
+#
+#
+# #6
+# result = 5*3
+# print("multiply is :",result)
+
+#sign can be used as the repatation of string
+# a="pythonQA automation "
+# b=a* 6
+# print(b)
+
+# / and // difference
+# result = 17/2
+# result1 = 17//2
+# print(result)
+# print(result1)
+
+result = 17%2
+print(result)
