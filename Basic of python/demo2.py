325458fc2dcdd5ad9ce15744f08466d8b34c590f
Justin <mhrjastin@gmail.com>
2024-09-13 16:38:06 +0545

    ifelse

---
+++ Basic of python/demo2.py
@@ -0,0 +1,34 @@
+
+
+# elseif
+# nested if else
+# function
+# OOP
+# constructor
+# import library module
+
+#elseif
+# => if you have more than 2 option
+
+# num = -5
+# if(num>0):
+#     print("number is positive")
+# elif(num<0):
+#     print("num is negative")
+# else:
+#     print("num is zero")
+
+
+#nested if else
+#if...else satement inside another if ...else
+
+
+
+num = float(input("Enter a number: "))
+if num >= 0:
+    if num == 0:
+        print("Zero")
+    else:
+        print("Positive")
+else:
+    print("Negative")
