38ba8c2cfb36497f2d0bbd8a91710279374e32a5
Justin <mhrjastin@gmail.com>
2024-09-16 21:54:59 +0545

    selenium packages

---
+++ Selenium/code1.py
@@ -0,0 +1,7 @@
+#import the necessary module
+#create browser chrome testing
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+import time
