1c045d75efa22e51d9a433989978e2f8e5960e4c
Justin <mhrjastin@gmail.com>
2024-09-16 22:09:45 +0545

    selenium 1st code runs successfully

--- Selenium/code1.py
+++ Selenium/code1.py
@@ -1,19 +1,31 @@
 #import the necessary module
 #create browser chrome testing
-
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 import time
 
 #set the chrome driver manager
-
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 
 #open the desire website
+website_url= "https://www.mindrisers.com.np/"
 
+#open the website
+driver.get(website_url)
+time.sleep(5)
 
+#maximize the window size
+driver.maximize_window()
+time.sleep(5)
 
+#extract the website title
+website_title=driver.title
+print(f"Website Title is: {website_title}")
+print("code execute successfully")
+
+#close the webdriver instance
+driver.quit()
 
 
 
