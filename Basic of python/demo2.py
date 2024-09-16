3d3ec8862195889bf03949d1621ff3ef6044127a
Justin <mhrjastin@gmail.com>
2024-09-16 21:48:44 +0545

    oop class calender

--- Basic of python/demo2.py
+++ Basic of python/demo2.py
@@ -33,14 +33,81 @@
 # else:
 #     print("Negative")
 
-#function
-
+#function : a block of code which runs when calls
 #def func_name(parameter)
 
-def abc(name):
-    print("hello",name)
-abc("python")
-abc(3566)
-abc("justin")
-abc("JAMES")
-abc("sane")
+
+# def abc(name):
+#     print("hello",name)
+# abc("python")
+# abc(3566)
+# abc("justin")
+# abc("JAMES")
+# abc("sane")
+
+
+# #4.
+# def add(num1,num2):
+#     return (num1 +num2)
+# print(add(2,5))
+# print(add(5,9))
+
+
+
+#build in functions
+# print(), input(), range()
+#variable
+#
+# x=10
+# def fun():
+#     x=5
+#     print("Value of x inside function: ",x)
+# fun()
+#
+# print("Value of x outside function: ",x)
+
+#oop => object oriented program
+#class
+#class is the collection of data and functions
+#object is the instant of a class
+
+
+# class myclass:
+#     a=800
+#
+#     def fun1(self):
+#         print("hello")
+# obj=myclass()
+# obj.fun1()
+# print(obj.a)
+#
+
+
+
+#import module
+import calendar
+yy=2024
+mm=2
+print(calendar.month(yy,mm))
+
+import calendar
+yy=2023
+mm=12
+print(calendar.month(yy,mm))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
