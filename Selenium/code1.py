bf25986b7c249f3e9bc614d9b19c6dff9b56e07a
Justin <mhrjastin@gmail.com>
2024-09-16 21:58:45 +0545

    selenium packages

--- Selenium/code1.py
+++ Selenium/code1.py
@@ -5,3 +5,17 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 import time
+
+#set the chrome driver manager
+
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+
+#open the desire website
+
+
+
+
+
+
+
+
