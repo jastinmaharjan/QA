5f39c218142ec115be988a9ad466c0f84def8911
Justin <mhrjastin@gmail.com>
2024-10-10 21:34:55 +0545

    put for random email

---
+++ Selenium/API/demo5put.py
@@ -0,0 +1,65 @@
+# put request
+from http.client import responses
+
+import requests
+import json
+import random
+import string
+
+# base url
+base_url = "https://reqres.in/api"
+# auth key:
+auth_token="token ....token value"
+
+# get the random generate email
+def generate_random_email():
+    domain="gmail.com"
+    email_length=5
+    random_string=''.join(random.choice(string.ascii_lowercase) for _ in range(email_length))
+    email=random_string +"@" +domain
+    return email
+
+# put function
+def put_reqest(user_id):
+    url=base_url +f"/users/{user_id}"
+    print("Put url:"+url)
+    headers={"Authorization":auth_token}
+    data={
+        "Name":"Justin",
+        "email":generate_random_email(),
+        "phone_number":"986159",
+        "job":"quality assurance"
+    }
+    response = requests.put(url, json=data, headers=headers)
+    assert response.status_code == 200
+    json_data = response.json()
+    json_str = json.dumps(json_data, indent=4)
+    print("Json Get request body{page}", json_str)
+    print("====Put method is done successfully====")
+
+# call the function
+put_reqest(86)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
