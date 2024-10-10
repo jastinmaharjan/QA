8176c2f1688377f78ea9d780e1a08c151bbc907b
Justin <mhrjastin@gmail.com>
2024-10-10 18:44:38 +0545

    Fetching Paginated Data using GET Requests with Authorization in Python

--- Selenium/API/demo2.py
+++ Selenium/API/demo2.py
@@ -1,3 +1,5 @@
+# Fetching Paginated Data using GET Requests with Authorization in Python
+
 # get request:
 import requests
 import json
@@ -8,10 +10,9 @@ base_url = "https://reqres.in/api"
 # auth key:
 auth_token="token ....token value"
 
-def get_request():
-    url=base_url +"/users/"
-    print("get url:"+url)
-    headers={"Authorization":auth_token}
+def get_request(page):
+    url=f"{base_url}/users?page={page}"
+    headers={"Authorization":auth_token} if auth_token else ()
     response= requests.get(url,headers=headers)
     assert  response.status_code ==200
     json_date=response.json()
@@ -20,5 +21,6 @@ def get_request():
     print("====Get method is done successfully=====")
 
 # call the function
-get_request()
+get_request(2)
+
 
