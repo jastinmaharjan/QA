19ca5624ebe1199ae3db263dfd4c08b3aae21962
Justin <mhrjastin@gmail.com>
2024-10-12 07:33:17 +0545

    Pytest to open  website

---
+++ Selenium/pytest/demo1_test.py
@@ -0,0 +1,25 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+
+def test_google_search():
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    driver.implicitly_wait(10)
+    driver.get("https://google.com")
+    search_box=driver.find_element(*(By.XPATH,"//textarea[@id='APjFqb']"))
+    search_box.send_keys("mindrisers.com.np")
+    search_box.send_keys(Keys.RETURN)
+    time.sleep(3)
+
+    # search and click on the link
+    first_link=driver.find_element(*(By.XPATH,"//h3[contains(text(),'Best IT Training Institute in kathmandu, Nepal | M')]"))
+    first_link.click()
+    driver.maximize_window()
+    time.sleep(5)
+    print("===pytest execute successfully===")
+
+
